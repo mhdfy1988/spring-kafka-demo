4aead9455d04b80db973b40ddf7ed23a64e25893
mhdfy1988 <luojiawen7000@126.com>
2021-03-10 01:51:09 +0800

    手动提交offset

--- src/main/java/com/ly/kafka/components/Consmer.java
+++ src/main/java/com/ly/kafka/components/Consmer.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jms.JmsProperties.AcknowledgeMode;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.KafkaListeners;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
@@ -116,5 +118,23 @@ public class Consmer {
     			log.info("------------------message : " + message);       
     		}  
         });
+	}
+	
+	/**
+	 * 批量消费 手动提交
+	 * @param records
+	 */
+	@KafkaListener(groupId="testGroup-5",  topics = "test",containerFactory="batchAndNoAutoFactory")
+    public void consumerBatchAndNoAuto(ConsumerRecords<String, String> records,Acknowledgment ack){
+        log.info("接收到消息数量：{}",records.count());
+        records.forEach((ConsumerRecord<String, String> record) -> {
+    		Optional<?> kafkaMessage = Optional.ofNullable(record.value());        
+    		if (kafkaMessage.isPresent()) {            
+    			Object message = kafkaMessage.get();            
+    			log.info("----------------- record :" + record);            
+    			log.info("------------------message : " + message);       
+    		}  
+        });
+        ack.acknowledge();
 	}
 }
--- src/main/java/com/ly/kafka/configuration/KafkaConfiguration.java
+++ src/main/java/com/ly/kafka/configuration/KafkaConfiguration.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 public class KafkaConfiguration {
 	
+	/**
+	 * 批量
+	 * @param consumerFactory
+	 * @return
+	 */
 	@Bean
 	public KafkaListenerContainerFactory<?> batchFactory(ConsumerFactory consumerFactory){
 	    ConcurrentKafkaListenerContainerFactory<Integer,String> factory =
@@ -17,6 +23,24 @@ public class KafkaConfiguration {
 	    factory.setConcurrency(10);
 	    factory.getContainerProperties().setPollTimeout(1500);
 	    factory.setBatchListener(true);//设置为批量消费，每个批次数量在Kafka配置参数中设置
+	    return factory; 
+	}
+	
+	/**
+	 * 批量且手动提交
+	 * @param consumerFactory
+	 * @return
+	 */
+	@Bean
+	public KafkaListenerContainerFactory<?> batchAndNoAutoFactory(ConsumerFactory consumerFactory){
+	    ConcurrentKafkaListenerContainerFactory<Integer,String> factory =
+	    new ConcurrentKafkaListenerContainerFactory<>();
+	    factory.setConsumerFactory(consumerFactory);
+	    factory.setConcurrency(10);
+	    factory.getContainerProperties().setPollTimeout(1500);
+	    factory.setBatchListener(true);//设置为批量消费，每个批次数量在Kafka配置参数中设置
+	    //设置手动提交模式
+	    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
 	    return factory; 
 	}
 	
