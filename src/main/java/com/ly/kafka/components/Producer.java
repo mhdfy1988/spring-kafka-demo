dc7abf9991c509e03f670a4bfedf61db64f839f9
mhdfy1988 <luojiawen7000@126.com>
2021-03-09 17:24:12 +0800

    批量生产

--- src/main/java/com/ly/kafka/components/Producer.java
+++ src/main/java/com/ly/kafka/components/Producer.java
@@ -1,5 +1,7 @@
 package com.ly.kafka.components;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,21 @@ public class Producer {
 	@Autowired
 	private KafkaTemplate<String,String>  template;
 	
+	/**
+	 * 单条生产
+	 * @param topic
+	 * @param msg
+	 */
 	public void sendMsg(String topic,String msg) {
 		template.send(topic, msg);
+	}
+	
+	/**
+	 * 批量生产
+	 * @param topic
+	 * @param msgList
+	 */
+	public void sendMsg(String topic,List<String> msgList) {
+		template.send(topic, msgList.toString());
 	}
 }
